691a9ae9f04caca67780d2164b499d45a915a1e2
adg3748 <revivedtomorrow@gmail.com>
2018-01-05 19:47:10 +0900

    complete

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,36 +1,32 @@
-while true
-  puts "任意整数を入力してください"
+def fizzbuzz(input)
+  input = input.to_i
+  if input!=0
+    if (input%3==0) && (input%5==0)
+      puts 'fizzbuzz'
+    elsif input%3==0
+      puts 'fizz'
+    elsif input%5==0
+      puts 'buzz'
+    else
+      puts input
+    end
+  else
+    puts input
+  end
+end
+
+while true do
+  puts "任意の整数を入力してください"
   input = gets.chomp
-  p input
   if input =~ /^[0-9]+$/ 
     # ^先頭を直後の文字で検索
     # []括弧のいずれか
     # +1つ以上直前の文字が続く
     # $末尾を直前の文字で検索
-    puts '正常な値です'
+    fizzbuzz(input)
+    break
   else
-    puts 'もう一度入力してください'
+    puts '不正な値です。もう一度入力してください。'
   end
 end
-
-# def fizzbuzz(input)
-#   while true do
-#     if (input%3==0) && (input%5==0)
-#       puts fizzbuzz
-#       break
-#     elsif input%3==0
-#       puts fizz
-#       break
-#     elsif input%5==0
-#       puts fizzbuzz
-#       break
-#     else
-#       puts "不正な値です。もう一度入力してください"
-#     end
-#   end
-# end
-# fizzbuzz(input)
-
-
-
 
